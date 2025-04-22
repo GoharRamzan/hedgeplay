db2c03e072858568280755384762abfe0d4fe062
GoharRamzan <goharnaqshbandi@gmail.com>
2025-04-22 15:23:06 +0500

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,15 +17,15 @@ const Hero = () => {
 
       {/* Dark Overlay */}
       <div className="absolute inset-0  md:top-24 flex   items-center justify-center">
-        <div className="text-center text-white px-4 w-[50%] md:w-[60%] overflow-hidden flex flex-col">
+        <div className="text-center text-white px-4 w-[100%] md:w-[60%] overflow-hidden flex flex-col">
           <h1 className="hidden sm:block text-5xl font-bold mb-4 leading-14 ">Smart&nbsp;<span className='bg-[#1A325E] rounded-xl  px-2'>Server</span>&nbsp;Based <br /> Hedging App</h1>
           {/* this is for mobile */}
-          <h1 className="block sm:hidden text-2xl  font-bold mb-4  ">Smart&nbsp;<span className='bg-[#1A325E] rounded-xl  px-2'>Server</span>&nbsp;<br /> Based Hedging App</h1>
+          <h1 className="block sm:hidden text-4xl  font-bold mb-4  ">Smart&nbsp;<span className='bg-[#1A325E] rounded-xl  px-2'>Server</span>&nbsp;<br /> Based Hedging App</h1>
           {/* //// */}
-          <p className="text-md md:text-xl mb-6 ">Stay Ahead. Trade smarter. Hedge your way to success.</p>
+          <p className="text-lg md:text-xl mb-6 ">Stay Ahead. Trade smarter. Hedge your way to success.</p>
         
         </div>
-        <div className='w-[50%] md:w-[40%] flex justify-center items-center'>
+        <div className='hidden md:flex w-[50%] md:w-[40%]  justify-center items-center'>
             <img src={image} alt="" className='w-[100%] md:w-[70%]' />
         </div>
       </div>
