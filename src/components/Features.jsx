081454a733c599c3f920371f56d5bf201ace6227
GoharRamzan <goharnaqshbandi@gmail.com>
2025-04-22 16:23:02 +0500

    Update Features.jsx

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,7 +4,7 @@ const Features = () => {
     return (
         <div className='bg-[#1A325E] h-full flex flex-col items-center w-full overflow-hidden'>
             <h1 className='bg-[#1C52CB] text-white font-bold h-12 mb-10   px-10 text-2xl flex items-center justify-center mt-7 rounded-3xl'>FEATURES</h1>
-            <div className='flex justify-between items-center w-[80%] h-full bg-[#a9a9b9] rounded-3xl py-9 px-11'>
+            <div className='flex justify-between items-center w-[94%] md:w-[80%] h-full bg-[#a9a9b9] rounded-3xl py-9 px-10'>
                 <ul className='list-disc text-xl md:text-2xl leading-10 w-full'>
                     <li>Advanced Algorithm.</li>
                     <li>Smart decisions making.</li>
