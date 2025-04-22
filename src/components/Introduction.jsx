b5d7f7a82df88203120993641d1ce7a6471394c1
GoharRamzan <goharnaqshbandi@gmail.com>
2025-04-22 16:25:18 +0500

    Update Introduction.jsx

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -6,7 +6,7 @@ const Introduction = () => {
         <div className='bg-[#a9a9b9] h-full flex flex-col items-center w-full overflow-hidden'>
             <h1 className='bg-[#1C52CB] text-white font-bold h-12 mb-10   px-4 text-2xl flex items-center justify-center mt-7 rounded-3xl'>INTRODUCTION</h1>
             <div className='flex justify-between items-center w-full h-full'>
-                <div className='w-full md:w-[50%]  flex justify-center items-center text-lg'>
+                <div className='w-full md:w-[50%]  flex justify-center items-center text-lg pb-10 md:p-0'>
                     <p className='w-[70%] text-lg md:text-2xl'>
                     Welcome to HedgePlay a cutting-edge Hedging advanced algorithm platform designed for serious traders. Whether you're Assest managing, or automating your strategies, HedgePlay offers a powerful, seamless experience. Built for speed, precision, and performance.
                     Already working with 1 Billion Daily Traders. Accuracy 80% - 90%
