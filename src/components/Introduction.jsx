dc6aaf67f86868bb22798eb0073610c410cec5f4
GoharRamzan <goharnaqshbandi@gmail.com>
2025-04-22 15:20:39 +0500

    Update Introduction.jsx

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -4,15 +4,15 @@ import '../styles/style.css'
 const Introduction = () => {
     return (
         <div className='bg-[#D3D6DD] h-full flex flex-col items-center w-full overflow-hidden'>
-            <h1 className='bg-[#1C52CB] text-white font-bold h-12 mb-10 w-[20%] text-2xl flex items-center justify-center mt-7 rounded-3xl'>INTRODUCTION</h1>
+            <h1 className='bg-[#1C52CB] text-white font-bold h-12 mb-10   px-4 text-2xl flex items-center justify-center mt-7 rounded-3xl'>INTRODUCTION</h1>
             <div className='flex justify-between items-center w-full h-full'>
-                <div className='w-[50%]  flex justify-center items-center text-lg'>
-                    <p className='w-[70%] text-2xl'>
+                <div className='w-full md:w-[50%]  flex justify-center items-center text-lg'>
+                    <p className='w-[70%] text-lg md:text-2xl'>
                     Welcome to HedgePlay a cutting-edge Hedging advanced algorithm platform designed for serious traders. Whether you're Assest managing, or automating your strategies, HedgePlay offers a powerful, seamless experience. Built for speed, precision, and performance.
                     Already working with 1 Billion Daily Traders. Accuracy 80% - 90%
                     </p>
                 </div>
-                <div className='w-[50%]  h-full overflow-hidden'>
+                <div className='hidden md:block w-[50%]  h-full overflow-hidden'>
                     <img src={image} alt="" className=" h-full w-full object-fill object clip-diagonal" />
                 </div>
             </div>
